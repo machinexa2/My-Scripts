e6b208be4122b6ca690cd091902c4f1ae866a89f
machinexa <machinexa@gmail.com>
2020-09-19 22:48:51 +0545

    10:48:51 PM 09/19/2020

--- SyncBackup/BackupKiller.sh
+++ SyncBackup/BackupKiller.sh
@@ -1,11 +1,11 @@
 while true
-if [[ `cat /tmp/KaliLinuxBackup` -eq 1 ]]; then
-	echo "Not killed! Waiting"
-	sleep 0.2
-else
-	echo "Killing"
-	kill `pgrep -f SyncBackup_Kali-Linux.sh` 
-	exit
-fi
-
-
+do
+	if [[ `cat /tmp/KaliLinuxBackup` -eq 1 ]]; then
+		echo "Not killed! Waiting"
+		sleep 0.2
+	else
+		echo "Killing"
+		kill "`pgrep -f SyncBackup.sh`" 
+		break
+	fi
+done
