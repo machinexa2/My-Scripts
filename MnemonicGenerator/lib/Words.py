597f0fead8baedec1857a9c428e5ed329432e26c
machinexa2 <machinexa>
2020-12-31 15:18:43 +0545

    03:18:43 PM 12/31/2020

--- MnemonicGenerator/lib/Words.py
+++ MnemonicGenerator/lib/Words.py
@@ -6,7 +6,7 @@ class Words:
         self.generate_single()
 
     def generate_single(self):
-        words = (line.rstrip('\n') for line in open('/root/MachineYadav/My-Scripts/MnemonicGenerator/words'))
+        words = (line.rstrip('\n') for line in open('/root/MachineYadav/My-Scripts/MnemonicGenerator/words')) #change it
         self.words_dict = {letter: [] for letter in ascii_uppercase}
         for word in words:
             self.words_dict[word[0].upper()].append(word)
